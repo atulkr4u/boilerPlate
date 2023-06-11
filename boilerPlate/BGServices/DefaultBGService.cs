a4d071e32cd26396dd211456f4704411a83f2f83
Atul Kumar <atulkr4u@hotmail.com>
2023-06-11 19:33:44 +0530

    Added Background Service

---
+++ boilerPlate/BGServices/DefaultBGService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using boilerPlate.Controllers;
+using boilerPlate.Infra.ServiceContracts;
+
+namespace boilerPlate.BGServices
+{
+	public class DefaultBGService : BackgroundService
+	{
+        private readonly ILogger<DefaultBGService> _logger;
+        IConfigService _config;
+        public DefaultBGService(ILogger<DefaultBGService> logger, IConfigService config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogCritical("Service Started!");
+            
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogCritical("Service Stopped!");
+            return Task.CompletedTask;
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogCritical($"Guided App Service Running:{DateTimeOffset.Now}");
+                await Task.Delay(36000, stoppingToken);
+            }
+        }
+    }
+}
+
